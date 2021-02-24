e8834328a4f602be7a745339c614999bea8cc40c
gulchichi <tjaqpfflsk@gmail.com>
2021-02-24 15:30:30 +0900

    master #3

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//#3 수정
 //#2 수정
 @Controller
 public class HomeController {
