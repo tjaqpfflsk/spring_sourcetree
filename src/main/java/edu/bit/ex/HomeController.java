bd55e8b433302fd1cb04e0801f92ab87c214c491
gulchichi <tjaqpfflsk@gmail.com>
2021-02-24 15:28:32 +0900

    master #2

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,9 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+//#2 ¼öÁ¤
 @Controller
 public class HomeController {
 	
