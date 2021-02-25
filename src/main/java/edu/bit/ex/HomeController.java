8a5a2f7f16e8bcaf75826259f5c28b4a051c3a34
gulchichi <tjaqpfflsk@gmail.com>
2021-02-25 11:50:49 +0900

    delete#1

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//#6
 //#2
 //#1
 //#5
